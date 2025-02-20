fccd126d188fcf099c033fa4930613b626490785
guing2003 <guilhermedelecrode1710@gmail.com>
2025-02-20 10:08:35 -0300

    Adiciona Cards e formatação de Valores

--- app/src/main/java/com/guilhermedelecrode/orgs/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/com/guilhermedelecrode/orgs/adapter/ListaProdutosAdapter.kt
@@ -1,11 +1,15 @@
 package com.guilhermedelecrode.orgs.adapter
 
 import android.content.Context
+
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.guilhermedelecrode.orgs.databinding.ProdutoItemBinding
 import com.guilhermedelecrode.orgs.model.Produto
+import java.math.BigDecimal
+import java.util.Locale
+import java.text.NumberFormat
 
 class ListaProdutosAdapter(
     produtos: List<Produto>,
@@ -14,13 +18,26 @@ class ListaProdutosAdapter(
 
     private val produtos = produtos.toMutableList()
 
-    class ViewHolder(private val binding: ProdutoItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(private val binding: ProdutoItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun vincula(produto: Produto) {
-            binding.produtoItemNome.text = produto.nome
-            binding.produtoItemDescricao.text = produto.descricao
-            binding.produtoItemValor.text = produto.valor.toPlainString()
+            val nome = binding.produtoItemNome
+            nome.text = produto.nome
+            val descricao = binding.produtoItemDescricao
+            descricao.text = produto.descricao
+            val valor = binding.produtoItemValor
+
+            val valorEmMoeda: String = formataParaMoedaBrasileira(produto.valor)
+            valor.text = valorEmMoeda
+        }
+        private fun formataParaMoedaBrasileira(valor: BigDecimal): String {
+            val formatador: NumberFormat = NumberFormat
+                .getCurrencyInstance(Locale("pt", "br"))
+            return formatador.format(valor)
         }
     }
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(context)
