c4209159b1b41ba541e8ef3ae04d6e1bce1ae837
guing2003 <guilhermedelecrode1710@gmail.com>
2025-02-28 09:45:42 -0300

    Carregando Imagem no formulario

--- app/src/main/java/com/guilhermedelecrode/orgs/ui/FormularioProdutoActivity.kt
+++ app/src/main/java/com/guilhermedelecrode/orgs/ui/FormularioProdutoActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
 import com.guilhermedelecrode.orgs.DAO.ProdutosDAO
-import com.guilhermedelecrode.orgs.R
 import com.guilhermedelecrode.orgs.databinding.ActivityFormularioProdutoBinding
+import com.guilhermedelecrode.orgs.databinding.FormularioImagemBinding
 import com.guilhermedelecrode.orgs.model.Produto
 import java.math.BigDecimal
 
@@ -19,17 +20,23 @@ class FormularioProdutoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Inflando o layout usando ViewBinding
         binding = ActivityFormularioProdutoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         configuraBotaoSalvar()
 
         binding.activityFormularioprodutoImagem.setOnClickListener{
+            val bindingFormularioImagem = FormularioImagemBinding.inflate(layoutInflater)
+            bindingFormularioImagem.formularioImagemBotaoCarregar.setOnClickListener {
+                val url = bindingFormularioImagem.formularioTextUrl.text.toString()
+                bindingFormularioImagem.formularioImagemImageview.load(url)
+            }
             AlertDialog.Builder(this)
-                .setView(R.layout.formulario_imagem)
+                .setView(bindingFormularioImagem.root)
                 .setPositiveButton("Sim") {_,_ ->
+                    val url = bindingFormularioImagem.formularioTextUrl.text.toString()
+                    binding.activityFormularioprodutoImagem.load(url)
+
 
                 }
                 .setNegativeButton("Não"){_,_ ->
