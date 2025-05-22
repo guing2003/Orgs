be7ab01e354eb4222116581ac76afb27f5fbba75
guing2003 <guilhermedelecrode1710@gmail.com>
2025-05-22 17:12:58 -0300

    Salvando e Buscando Produtos no banco de dados

--- app/src/main/java/com/guilhermedelecrode/orgs/ui/ListaProdutosActivity.kt
+++ app/src/main/java/com/guilhermedelecrode/orgs/ui/ListaProdutosActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.guilhermedelecrode.orgs.DAO.ProdutosDAO
 import com.guilhermedelecrode.orgs.adapter.ListaProdutosAdapter
+import com.guilhermedelecrode.orgs.database.AppDatabase
 import com.guilhermedelecrode.orgs.databinding.ActivityListaProdutosBinding
+import com.guilhermedelecrode.orgs.model.Produto
+import java.math.BigDecimal
 
 class ListaProdutosActivity : AppCompatActivity() {
     private val dao = ProdutosDAO()
@@ -21,15 +25,36 @@ class ListaProdutosActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // Inflando o layout usando ViewBinding
         binding = ActivityListaProdutosBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         configuraRecyclerView()
-
         configuraFab()
 
+        val db = Room.databaseBuilder(
+            this,
+            AppDatabase::class.java,
+            "org,db"
+        ).allowMainThreadQueries()
+            .build()
+
+        val produtoDao = db.produtoDao()
+
+        produtoDao.salva(
+            Produto(
+                nome = "Teste Nome 1",
+                descricao = "Teste descricao 1",
+                valor = BigDecimal("10.0")
+            )
+        )
+        adapter.atualiza(produtoDao.buscaTodos())
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //adapter.atualiza(dao.buscaTodos())
+        Log.i("ListaProduto", "onResume: ${dao.buscaTodos()}")
     }
 
     private fun configuraRecyclerView() {
@@ -37,7 +62,7 @@ class ListaProdutosActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
         adapter.quandoClicaNoItemlistener = {
-                Log.i("ListaProdutos", "quandoClica: ${it.nome}")
+            Log.i("ListaProdutos", "quandoClica: ${it.nome}")
             val intent = Intent(this, DetalhesProdutoActivity::class.java).apply {
                 putExtra("produto", it)
             }
@@ -45,11 +70,7 @@ class ListaProdutosActivity : AppCompatActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        adapter.atualiza(dao.buscaTodos())
-        Log.i("ListaProduto", "onResume: ${dao.buscaTodos()}")
-    }
+
 
     private fun configuraFab() {
         val fab = binding.activityListaProdutoExtendedFab
